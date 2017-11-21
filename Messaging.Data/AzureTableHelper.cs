ea875ee8a5b7f06fb0b45861aec8456380fe5214
Vít Svoboda <vits@kentico.com>
2017-11-21 17:35:43 +0100

    Extract table selection by prefix
    So it can be reused.

---
+++ Messaging.Data/AzureTableHelper.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Messaging.Data
+{
+    internal class AzureTableHelper
+    {
+        public static async Task<IEnumerable<T>> GetSegmentedResult<T>(CloudTable table, TableQuery<T> query)
+            where T : ITableEntity, new()
+        {
+            TableContinuationToken continuationToken = null;
+            var items = new List<T>();
+            do
+            {
+                var segment = await table.ExecuteQuerySegmentedAsync(query, continuationToken);
+                if (segment != null)
+                {
+                    continuationToken = segment.ContinuationToken;
+                    items.AddRange(segment.Results);
+                }
+            } while (continuationToken != null);
+
+            return items;
+        }
+
+        public static TableQuery<T> GetRowKeyPrefixQuery<T>(string partitionKey, string rowKeyPrefix)
+            where T : ITableEntity, new()
+        {
+            var query = new TableQuery<T>();
+
+            var length = rowKeyPrefix.Length - 1;
+            var lastChar = rowKeyPrefix[length];
+
+            var nextLastChar = (char)(lastChar + 1);
+
+            var startsWithEndPattern = rowKeyPrefix.Substring(0, length) + nextLastChar;
+
+            var prefixCondition = TableQuery.CombineFilters(
+                TableQuery.GenerateFilterCondition("RowKey",
+                    QueryComparisons.GreaterThanOrEqual,
+                    rowKeyPrefix),
+                TableOperators.And,
+                TableQuery.GenerateFilterCondition("RowKey",
+                    QueryComparisons.LessThan,
+                    startsWithEndPattern)
+            );
+
+            var filterString = TableQuery.CombineFilters(
+                TableQuery.GenerateFilterCondition("PartitionKey",
+                    QueryComparisons.Equal,
+                    partitionKey),
+                TableOperators.And,
+                prefixCondition
+            );
+
+            return query.Where(filterString);
+        }
+    }
+}
--- Messaging.Data/Repositories/MessageRepository.cs
+++ Messaging.Data/Repositories/MessageRepository.cs
@@ -20,38 +20,6 @@ namespace Messaging.Data.Repositories
                 .GetTableReference("DataTable");
         }
 
-        private TableQuery<T> GetWithinPartitionStartsWithByRowKeyQuery<T>(string partitionKey, string startsWithPattern) where T : ITableEntity, new()
-        {
-            var query = new TableQuery<T>();
-
-            var length = startsWithPattern.Length - 1;
-            var lastChar = startsWithPattern[length];
-
-            var nextLastChar = (char) (lastChar + 1);
-
-            var startsWithEndPattern = startsWithPattern.Substring(0, length) + nextLastChar;
-
-            var prefixCondition = TableQuery.CombineFilters(
-                TableQuery.GenerateFilterCondition("RowKey",
-                    QueryComparisons.GreaterThanOrEqual,
-                    startsWithPattern),
-                TableOperators.And,
-                TableQuery.GenerateFilterCondition("RowKey",
-                    QueryComparisons.LessThan,
-                    startsWithEndPattern)
-            );
-
-            var filterString = TableQuery.CombineFilters(
-                TableQuery.GenerateFilterCondition("PartitionKey",
-                    QueryComparisons.Equal,
-                    partitionKey),
-                TableOperators.And,
-                prefixCondition
-            );
-
-            return query.Where(filterString);
-        }
-
         private async Task<MessageEntity> GetEntity(Guid appId, Guid channelId, Guid messageId)
         {
             var existingResult = await _table.ExecuteAsync(TableOperation.Retrieve<MessageEntity>(appId.ToString(), GetRowKey(channelId, messageId)));
@@ -67,20 +35,11 @@ namespace Messaging.Data.Repositories
 
         public async Task<IEnumerable<Message>> GetAll(Guid appId, Guid channelId, int lastN)
         {
-            var query = GetWithinPartitionStartsWithByRowKeyQuery<MessageEntity>(appId.ToString(), RowKeyPrefix + channelId);
-            TableContinuationToken continuationToken = null;
-            var items = new List<Message>();
-            do
-            {
-                var segment = await _table.ExecuteQuerySegmentedAsync(query, continuationToken);
-                if (segment != null)
-                {
-                    continuationToken = segment.ContinuationToken;
-                    items.AddRange(segment.Select(ToDto));
-                }
-            } while (continuationToken != null);
+            var query = AzureTableHelper.GetRowKeyPrefixQuery<MessageEntity>(appId.ToString(), RowKeyPrefix + channelId);
 
-            return items;
+            var entities = await AzureTableHelper.GetSegmentedResult(_table, query);
+
+            return entities.Select(ToDto).ToList();
         }
 
         public async Task<Message> Upsert(Guid appId, Guid channelId, Message message)
