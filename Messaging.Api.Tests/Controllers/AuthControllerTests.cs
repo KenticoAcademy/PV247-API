4013b89a630bafdcf8c3bebf667d079916a329cd
Vít Svoboda <vits@kentico.com>
2018-08-24 17:22:18 +0200

    Add controller tests

---
+++ Messaging.Api.Tests/Controllers/AuthControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Messaging.Contract.Repositories;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using Xunit;
+
+namespace Messaging.Api.Tests.Controllers
+{
+    public class AuthControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly IUserRepository _userRepositoryMock;
+
+        public AuthControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _userRepositoryMock = Substitute.For<IUserRepository>();
+
+            _factory = factory.WithWebHostBuilder(builder => builder
+                .UseSetting("TokenSigningKey", "asdfasdfasdfasdf")
+                .ConfigureTestServices(services => services.AddScoped(_ => _userRepositoryMock)));
+        }
+
+        [Fact]
+        public async Task Login_ExistingUser_Ok()
+        {
+            var email = "test@test.test";
+            var client = _factory.CreateClient();
+            _userRepositoryMock.IsValidUser(email).Returns(true);
+
+            var response = await client.PostAsync("/api/auth", new StringContent($"\"{email}\"", Encoding.UTF8, "application/json"));
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task Login_NonExistingUser_BadRequest()
+        {
+            var client = _factory.CreateClient();
+            _userRepositoryMock.IsValidUser(Arg.Any<string>()).Returns(false);
+
+            var response = await client.PostAsync("/api/auth", new StringContent($"\"test@test.test\"", Encoding.UTF8, "application/json"));
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
---
+++ Messaging.Api.Tests/WebApplicationFactoryExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Messaging.Contract.Repositories;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+
+namespace Messaging.Api.Tests
+{
+    public static class WebApplicationFactoryExtensions
+    {
+        public static async Task<HttpClient> CreateAuthenticatedClient(this WebApplicationFactory<Startup> factory,
+            IUserRepository userRepositoryMock = null, string emailToAuthenticate = "current@user.test")
+        {
+            var userRepository = userRepositoryMock ?? Substitute.For<IUserRepository>();
+            userRepository.IsValidUser(emailToAuthenticate).Returns(true);
+
+            var client = factory
+                .WithWebHostBuilder(builder => builder
+                    .UseSetting("TokenSigningKey", "asdfasdfasdfasdf")
+                    .ConfigureTestServices(services => services.AddScoped(_ => userRepository)))
+                .CreateClient();
+
+            var response = await client.PostAsync("/api/auth", new StringContent($"\"{emailToAuthenticate}\"", Encoding.UTF8, "application/json"));
+            var token = await response.EnsureSuccessStatusCode()
+                .Content.ReadAsStringAsync();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            return client;
+        }
+    }
+}
