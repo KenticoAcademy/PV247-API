ed3ca4f9d518c1a70352fdcb6423ea00cc90e28c
Vít Svoboda <vits@kentico.com>
2018-08-29 12:01:46 +0200

    Remove channels from the application response

---
+++ Messaging.Api/Models/ApplicationResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Messaging.Api.Models
+{
+    /// <summary>
+    /// Application response model
+    /// </summary>
+    public class ApplicationResponse
+    {
+        /// <summary>
+        /// Application ID
+        /// </summary>
+        public Guid Id { get; set; }
+    }
+}
